943e9318636585d65d02d83b20dd5c4073d984dc
iGxth <thitipong.hans@bumail.net>
2024-02-07 17:23:52 +0700

    finishTankGame

---
+++ WorkshopMultiplayer/Assets/Scripts/Core/ProjectileLaucher.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using System;
+
+public class ProjectileLaucher : NetworkBehaviour
+{
+    [Header("References")]
+    [SerializeField] private InputReader inputReader;
+    [SerializeField] private Transform projectileSpawnPoint;
+    [SerializeField] private GameObject severProjectilePrefab;
+    [SerializeField] private GameObject clientProjectilePrefab;
+    [SerializeField] private GameObject muzzleFlash;
+    [SerializeField] private Collider2D playerCollider;
+
+
+    [Header("Settings")]
+
+    [SerializeField] private float projectileSpeed;
+    [SerializeField] private float fireRate;
+    [SerializeField] private float muzzleFlashDuration;
+
+    private bool shouldFire;
+    private float previousFireTime;
+    private float muzzletFlashTimer;
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsOwner) { return; }
+        inputReader.PrimaryFireEvent += HandlePrimaryFire;
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (!IsOwner) { return; }
+        inputReader.PrimaryFireEvent -= HandlePrimaryFire;
+    }
+
+    private void HandlePrimaryFire(bool shouldFire)
+    {
+        this.shouldFire = shouldFire;
+    }
+
+    private void SpawnDummyProjectile(Vector3 spawnPos,Vector3 direction)
+    {
+        muzzleFlash.SetActive(true);
+        muzzletFlashTimer = muzzleFlashDuration;
+        
+        
+        GameObject projectileInstance = Instantiate(clientProjectilePrefab, spawnPos, Quaternion.identity);
+
+        projectileInstance.transform.up = direction;
+
+        Physics2D.IgnoreCollision(playerCollider, projectileInstance.GetComponent<Collider2D>());
+
+        if(projectileInstance.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+        {
+            rb.velocity = rb.transform.up * projectileSpeed;
+        }
+    }
+
+    private void Update()
+    {
+        
+        if(muzzletFlashTimer > 0f)
+        {
+            muzzletFlashTimer -= Time.deltaTime;
+
+            if(muzzletFlashTimer <= 0f)
+            {
+                muzzleFlash.SetActive(false);
+            }
+        }
+        
+        
+        if (!IsOwner) { return; }
+        if(!shouldFire) { return; }
+
+        if(Time.time < (1 / fireRate) + previousFireTime) { return; }
+
+        PrimaryFireServerRpc(projectileSpawnPoint.position, projectileSpawnPoint.up);
+        SpawnDummyProjectile(projectileSpawnPoint.position, projectileSpawnPoint.up);
+
+        previousFireTime = Time.time;
+
+    }
+
+    [ServerRpc]
+    private void PrimaryFireServerRpc(Vector3 spawnPos, Vector3 direction)
+    {
+        GameObject projectileInstance = Instantiate(clientProjectilePrefab, spawnPos, Quaternion.identity);
+
+        projectileInstance.transform.up = direction;
+
+        Physics2D.IgnoreCollision(playerCollider, projectileInstance.GetComponent<Collider2D>());
+
+        if (projectileInstance.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+        {
+            rb.velocity = rb.transform.up * projectileSpeed;
+        } 
+
+        SpawnDummyProjectileClientRpc(spawnPos, direction);
+
+    }
+
+
+    [ClientRpc]
+    private void SpawnDummyProjectileClientRpc(Vector3 spawnPos, Vector3 direction)
+    {
+      if (!IsOwner) { return; }
+
+        SpawnDummyProjectile(spawnPos, direction);
+    }
+
+
+        
+    
+}
+
+
